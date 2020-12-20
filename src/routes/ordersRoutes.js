f0102c905f31ad8b9cb11d6400382d1a40b22852
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2020-12-20 12:48:34 -0500

    orders routes and service done
    fix order model

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import error from 'http-errors';
+import _ from 'lodash';
+
+import ordersService from '../services/ordersService.js'
 
 const router = express.Router();
+
+class OrdersRoutes {
+    constructor() {
+
+    }
+
+    
+}
 
 export default router;
