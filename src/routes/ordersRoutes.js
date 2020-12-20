7263ae80a1693003e8ba0e000073a47f9f90fe30
NathanWorkOnline <48111192+NathanWorkOnline@users.noreply.github.com>
2020-12-20 12:58:19 -0500

    GetOne commande

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -8,10 +8,30 @@ const router = express.Router();
 
 class OrdersRoutes {
     constructor() {
+        router.get('/:idOrder', this.getOne);
 
     }
 
+    async getOne(req, res, next) {
+        const idOrder = req.params.idOrder;
+
+        try {
+            let order = await ordersService.retrieveById(idOrder);
+
+            if (!order) {
+                return next(error.NotFound(`La commande avec l'identifiant ${idOrder} est introuvable.`));
+            }
+
+            order = order.toObject({ virtuals: true });
+            order = ordersService.transform(order);
+
+            res.status(200).json(order);
+        } catch (err) {
+            return next(err);
+        }
+    }
     
 }
+
 
 export default router;
