eba083c4c780743fddafe61d4ef2ed1293736cd9
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2020-12-20 12:56:07 -0500

    added getAll orders

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -8,10 +8,28 @@ const router = express.Router();
 
 class OrdersRoutes {
     constructor() {
-
+        router.get('/', this.getAll);
     }
 
-    
+    async getAll(req, res, next) {
+        const criteria = {};
+
+        try {
+            let orders = await ordersService.retrieveAll(criteria);
+
+            orders = orders.map((o) => {
+                o = o.toObject({ virtuals: true });
+                o = ordersService.transform(o);
+                return o;
+            });
+
+            res.status(200).json(orders);
+        } catch (err) {
+            return next(err);
+        }
+    }
 }
+
+new OrdersRoutes();
 
 export default router;
